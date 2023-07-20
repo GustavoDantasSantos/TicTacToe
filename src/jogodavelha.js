5f8239c02059a985e7f8c490d32e3e0b8d8df6fd
Gustavo-Vinicius-Santos <gustavo.vinicius.santos@ibm.com>
2023-07-20 12:15:12 -0300

    Criei teste para imprimir o tabuleiro

--- src/jogodavelha.js
+++ src/jogodavelha.js
@@ -1,5 +1,15 @@
 function jogoDaVelha(tabuleiro) {
+  imprimirTabuleiro(tabuleiro);
   return tabuleiro;
+}
+
+function imprimirTabuleiro(tabuleiro) {
+  console.log(`
+    ${tabuleiro[0]} | ${tabuleiro[1]} | ${tabuleiro[2]}
+    -----------
+    ${tabuleiro[3]} | ${tabuleiro[4]} | ${tabuleiro[5]}
+    -----------
+    ${tabuleiro[6]} | ${tabuleiro[7]} | ${tabuleiro[8]}`);
 }
 
 module.exports = {
--- test/jogodavelha.spec.js
+++ test/jogodavelha.spec.js
@@ -2,7 +2,12 @@ const { jogoDaVelha } = require('../src/jogodavelha');
 
 describe('XP Farm - Jogo da velha', () => {
   test('deve chamar a função jogoDaVelha e retorna o tabuleiro ', () => {
-    tabuleiro = ['', '', '', '', '', '', '', '', ''];
+    tabuleiro = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
     expect(jogoDaVelha(tabuleiro)).toStrictEqual(tabuleiro);
+  });
+  test('deve passar o numero 1 e o jogador X deve ser colocado na primeira casa do array ', () => {
+    tabuleiro = ['-', '-', '-', '-', '-', '-', '-', '-', '-'];
+    jogadorX = 1;
+    expect(jogoDaVelha(tabuleiro)).toStrictEqual(tabuleiro, jogadorX);
   });
 });
